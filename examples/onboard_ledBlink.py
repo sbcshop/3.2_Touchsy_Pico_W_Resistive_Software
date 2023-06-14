b47847bc0127d7a5c2e5b962217cf1c1603f7c6d
sbcshop3 <129948335+sbcshop3@users.noreply.github.com>
2023-06-14 16:19:25 +0530

    Create onboard_ledBlink.py

---
+++ examples/onboard_ledBlink.py
@@ -0,0 +1,16 @@
+from machine import Pin
+from time import sleep
+
+# "LED" or GP25 to select onboard led of Pico W 
+Led = Pin("LED", Pin.OUT) 
+
+while True:
+    Led.on()  #To switch on LED
+    sleep(2)  # wait 2 second
+    print("LED is ON")
+    
+    Led.off() #To switch off LED
+    sleep(2)  #wait 2 second
+    print("LED is OFF")
+    
+    
