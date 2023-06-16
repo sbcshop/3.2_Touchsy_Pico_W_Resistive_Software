80be4f046fd34263dc17c5a4d03c6aba18fb18ac
sbcshop3 <129948335+sbcshop3@users.noreply.github.com>
2023-06-16 12:32:24 +0530

    Update demo_SDcard.py

--- examples/demo_SDcard.py
+++ examples/demo_SDcard.py
@@ -13,20 +13,27 @@ import utime
 uart = UART(0,baudrate = 9600,tx = Pin(0),rx = Pin(1))
 
 def sdtest(data):
+    #define and configure SPI interfacing of sdcard with Pico
     spi=SPI(0,sck=Pin(18),mosi=Pin(19),miso=Pin(16))
     sd=sdcard.SDCard(spi,Pin(17))
+
+    #mount file system
     vfs = os.VfsFat(sd)
-    os.mount(vfs, "/fc")
+    os.mount(vfs, "/fc") 
     print("Filesystem check")
     print(os.listdir("/fc"))
 
+    #file name 
     fn = "/fc/File.txt"
     print()
     print("Single block read/write")
+
+    #write operation 
     with open(fn, "a") as f:
         n = f.write(data)  
         print(n, "bytes written")
 
+    #read operation
     with open(fn, "r") as f:
         result2 = f.read()
         print(len(result2), "bytes read")
